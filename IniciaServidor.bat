b8779c51a1658c93cc3753359ccbf5fa5078630f
Verde Perto Ambiental <ambientalverdeperto@gmail.com>
2025-12-24 16:10:27 -0300

    Add files via upload

---
+++ IniciaServidor.bat
@@ -0,0 +1,27 @@
+:: Arquivo BAT
+@echo off
+
+:: ================================
+:: CONFIGURACOES
+:: ================================
+set "RAIZ=C:\Verde Perto Ambiental"
+set "REPO=%RAIZ%\whatsapp"
+
+cls
+
+cd /d "%REPO%" || goto erro
+
+echo.
+echo ===========================
+echo    Iniciando projeto...
+echo ===========================
+echo.
+echo.
+echo ===================================
+echo   ATENCAO ATENCAO ATENCAO ATENCAO
+echo ===================================
+echo - ATENCAO: EVITE fechar esta janela pelo X da janela. 
+echo - UTILIZE CTRL+C e depois aperte S
+::echo Iniciando projeto...
+npm start
+
